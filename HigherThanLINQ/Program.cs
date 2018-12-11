f6d5735fd82b842804ba55e83b8305a4132e8895
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-12-11 19:06:07 +0100

    Add project files.

---
+++ HigherThanLINQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigherThanLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program asks the user for numbers than asks for additional specified value and displays the numbers higher than the specified value");
+            List<int> userList = new List<int>();
+            Console.WriteLine("Input the number of members on the List");
+            int amount = int.Parse(Console.ReadLine());
+            for (int i =0; i<= amount; i++)
+            {
+                Console.WriteLine("Number {0} : ", i);
+                int listMemer = int.Parse(Console.ReadLine());
+                userList.Add(listMemer);
+            }
+            Console.WriteLine("Input the value above which you want to display the members of the List : ");
+            int value = int.Parse(Console.ReadLine());
+
+            var result = from x in userList
+                         where x > value
+                         select x;
+
+            List<int> filterList = userList.FindAll(y => y > value);
+            Console.WriteLine("The numers greater than {0} are: ", value);
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("Additional check by LINQ method");
+            foreach (var n in filterList)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
